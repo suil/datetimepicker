3a2f72e52743bebcc737f47133256214bebc8fc9
Shuang Liu <gnauhsuil@live.com>
2015-09-01 23:13:12 -0500

    add keycode

--- src/js/eventBindings.ts
+++ src/js/eventBindings.ts
@@ -8,7 +8,25 @@ function bindEvents($input) {
 	var storage = $input.data("datetimepicker");
     var $calendar = storage.$calendar;
     var state: State = storage.state;
-
+    var keyCode = {
+        BACKSPACE: 8,
+        COMMA: 188,
+        DELETE: 46,
+        DOWN: 40,
+        END: 35,
+        ENTER: 13,
+        ESCAPE: 27,
+        HOME: 36,
+        LEFT: 37,
+        PAGE_DOWN: 34,
+        PAGE_UP: 33,
+        PERIOD: 190,
+        RIGHT: 39,
+        SPACE: 32,
+        TAB: 9,
+        UP: 38
+    };
+    
     // calendar dropdown showing event, also binds a one-time event to
     // close the dropdown
     $input.on("click.datetimepicker", () => {
